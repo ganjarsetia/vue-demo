8bb9b52997dab964a764325be952bc84cf893f72
Ganjar Setia <ganjar_setia@ymail.com>
2017-05-05 00:47:32 +0700

    create method on submit
    Signed-off-by: Ganjar Setia <ganjar_setia@ymail.com>

---
+++ src/GithubInput/script.js
@@ -0,0 +1,12 @@
+import bus from '../bus'
+
+export default {
+    name: 'GithubInput',
+    methods: {
+        onSubmit(event) {
+            if (this.username && this.username !== '') {
+                bus.$emit('new-username', this.username)
+            }
+        }
+    }
+}
